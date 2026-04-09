9beed19bfee13848bd6adc82d2a709184f3de435
jjlmoya <juanjo1989@gmail.com>
2026-04-09 19:09:41 +0200

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 export { textilesCategory } from './category';
 export { default as textilesCategorySEO } from './category/seo.astro';
 
-export { FabricTruthComponent, FabricTruthSEO, FabricTruthBibliography } from './tool/fabricTruth';
-export { LaundryGuideComponent, LaundryGuideSEO, LaundryGuideBibliography } from './tool/laundryGuide';
-export { StainChemistryComponent, StainChemistrySEO, StainChemistryBibliography } from './tool/stainChemistry';
-export { BurnTestComponent, BurnTestSEO, BurnTestBibliography } from './tool/burnTest';
-export { FiberPrepComponent, FiberPrepSEO, FiberPrepBibliography } from './tool/fiberPrep';
-export { ShoeSizeConverterComponent, ShoeSizeConverterSEO, ShoeSizeConverterBibliography } from './tool/shoeSizeConverter';
-export { ClothingSizeConverterComponent, ClothingSizeConverterSEO, ClothingSizeConverterBibliography } from './tool/clothingSizeConverter';
-export { KnittingGaugeComponent, KnittingGaugeSEO, KnittingGaugeBibliography } from './tool/knittingGauge';
-export { FabricProjectCalculatorComponent, FabricProjectCalculatorSEO, FabricProjectCalculatorBibliography } from './tool/fabricProjectCalculator';
-export { SewingPatternScalerComponent, SewingPatternScalerSEO, SewingPatternScalerBibliography } from './tool/sewingPatternScaler';
-export { NeedleConverterComponent, NeedleConverterSEO, NeedleConverterBibliography } from './tool/needleConverter';
-export { YarnCalculatorComponent, YarnCalculatorSEO, YarnCalculatorBibliography } from './tool/yarnCalculator';
+export { FabricTruthComponent, FabricTruthSEO, FabricTruthBibliography, FABRIC_TRUTH_TOOL } from './tool/fabricTruth';
+export { LaundryGuideComponent, LaundryGuideSEO, LaundryGuideBibliography, LAUNDRY_GUIDE_TOOL } from './tool/laundryGuide';
+export { StainChemistryComponent, StainChemistrySEO, StainChemistryBibliography, STAIN_CHEMISTRY_TOOL } from './tool/stainChemistry';
+export { BurnTestComponent, BurnTestSEO, BurnTestBibliography, BURN_TEST_TOOL } from './tool/burnTest';
+export { FiberPrepComponent, FiberPrepSEO, FiberPrepBibliography, FIBER_PREP_TOOL } from './tool/fiberPrep';
+export { ShoeSizeConverterComponent, ShoeSizeConverterSEO, ShoeSizeConverterBibliography, SHOE_SIZE_CONVERTER_TOOL } from './tool/shoeSizeConverter';
+export { ClothingSizeConverterComponent, ClothingSizeConverterSEO, ClothingSizeConverterBibliography, CLOTHING_SIZE_CONVERTER_TOOL } from './tool/clothingSizeConverter';
+export { KnittingGaugeComponent, KnittingGaugeSEO, KnittingGaugeBibliography, KNITTING_GAUGE_TOOL } from './tool/knittingGauge';
+export { FabricProjectCalculatorComponent, FabricProjectCalculatorSEO, FabricProjectCalculatorBibliography, FABRIC_PROJECT_CALCULATOR_TOOL } from './tool/fabricProjectCalculator';
+export { SewingPatternScalerComponent, SewingPatternScalerSEO, SewingPatternScalerBibliography, SEWING_PATTERN_SCALER_TOOL } from './tool/sewingPatternScaler';
+export { NeedleConverterComponent, NeedleConverterSEO, NeedleConverterBibliography, NEEDLE_CONVERTER_TOOL } from './tool/needleConverter';
+export { YarnCalculatorComponent, YarnCalculatorSEO, YarnCalculatorBibliography, YARN_CALCULATOR_TOOL } from './tool/yarnCalculator';
 
 export type {
   KnownLocale,
